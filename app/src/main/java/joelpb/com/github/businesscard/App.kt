86c483efe704f3481767c56d41c1002a626b33a8
JoelPB <coffee.program.joel@gmail.com>
2021-07-11 11:20:06 -0300

    Configuração do Room Database, parte 1

--- app/src/main/java/joelpb/com/github/businesscard/App.kt
+++ app/src/main/java/joelpb/com/github/businesscard/App.kt
@@ -1,7 +1,10 @@
 package joelpb.com.github.businesscard
 
 import android.app.Application
+import joelpb.com.github.businesscard.data.AppDatabase
+import joelpb.com.github.businesscard.data.BusinessCardRepository
 
 class App: Application() {
-
+    val database by lazy { AppDatabase.getDatabase(this) }
+    val repository by lazy { BusinessCardRepository(database.businessDao()) }
 }
