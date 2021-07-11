b200677206a82bc5a264809929767b40428cc790
JoelPB <coffee.program.joel@gmail.com>
2021-07-11 10:33:41 -0300

    Montando layout do projeto, parte 3

---
+++ app/src/main/java/joelpb/com/github/businesscard/ui/AddBusinessCardActivity.kt
@@ -0,0 +1,26 @@
+package joelpb.com.github.businesscard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import joelpb.com.github.businesscard.databinding.ActivityAddBusinessCardBinding
+
+class AddBusinessCardActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    private fun insertListeners(){
+        binding.btnClose.setOnClickListener {
+            finish()
+        }
+
+        binding.btnConfirm.setOnClickListener {
+
+        }
+    }
+}
--- app/src/main/java/joelpb/com/github/businesscard/ui/MainActivity.kt
+++ app/src/main/java/joelpb/com/github/businesscard/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package joelpb.com.github.businesscard.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import joelpb.com.github.businesscard.R
@@ -11,5 +12,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        insertListener()
+    }
+
+
+    private fun insertListener(){
+        binding.fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
