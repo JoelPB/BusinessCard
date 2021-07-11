d267bdb0a4d5d1b316705b8a91a49c1cbf8df43d
JoelPB <coffee.program.joel@gmail.com>
2021-07-11 17:30:47 -0300

    Configuração do Room Database, parte 3

--- app/src/main/java/joelpb/com/github/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/joelpb/com/github/businesscard/ui/AddBusinessCardActivity.kt
@@ -2,11 +2,21 @@ package joelpb.com.github.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import joelpb.com.github.businesscard.App
+import joelpb.com.github.businesscard.R
+import joelpb.com.github.businesscard.data.BusinessCard
 import joelpb.com.github.businesscard.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+
+    private val mainViewModel: MainViewModel by viewModels{
+        MainViewModelFactory((application as App).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -20,7 +30,16 @@ class AddBusinessCardActivity : AppCompatActivity() {
         }
 
         binding.btnConfirm.setOnClickListener {
-
+            val businessCard = BusinessCard(
+                nome = binding.tilNome.editText?.text.toString(),
+                empresa = binding.tilEmpresa.editText?.text.toString(),
+                telefone = binding.tilTelefone.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                fundoPersonalizado = binding.tilCor.editText?.text.toString()
+            )
+            mainViewModel.insert(businessCard)
+            Toast.makeText(this, R.string.label_show_success, Toast.LENGTH_SHORT).show()
+            finish()
         }
     }
 }
