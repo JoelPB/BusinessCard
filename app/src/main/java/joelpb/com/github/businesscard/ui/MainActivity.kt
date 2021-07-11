4eacb133721e9ec90754cd50f296f61cb956fc1e
JoelPB <coffee.program.joel@gmail.com>
2021-07-11 17:09:05 -0300

    Configuração do Room Database, parte 2

--- app/src/main/java/joelpb/com/github/businesscard/ui/MainActivity.kt
+++ app/src/main/java/joelpb/com/github/businesscard/ui/MainActivity.kt
@@ -3,24 +3,36 @@ package joelpb.com.github.businesscard.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import joelpb.com.github.businesscard.R
+import androidx.activity.viewModels
+import joelpb.com.github.businesscard.App
 import joelpb.com.github.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val mainViewModel: MainViewModel by viewModels{
+        MainViewModelFactory((application as App).repository)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
+        binding.rvCards.adapter = adapter
+        getAllBusinessCard()
         insertListener()
     }
 
+    private val adapter by lazy { BusinessCardAdapter() }
 
     private fun insertListener(){
         binding.fab.setOnClickListener {
             val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun getAllBusinessCard(){
+        mainViewModel.getAll().observe( this, { businessCard ->
+            adapter.submitList(businessCard)
+        })
     }
 }
